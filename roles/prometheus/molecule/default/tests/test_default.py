cb560b5323a1e1b282df77662abd627167c00d69
nickherrig <neherrig@gmail.com>
2020-08-30 12:35:43 -0500

    Init prometheus role with molecule v3

---
+++ roles/prometheus/molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_user(host):
+    assert host.group("prometheus").exists
+    assert host.user("prometheus").exists
