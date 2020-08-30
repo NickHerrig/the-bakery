f99b2d6290ef19b8621ebfe271cc9f9354154182
nickherrig <neherrig@gmail.com>
2020-08-30 17:34:14 -0500

    Add tests/db dir prometheus role

--- roles/prometheus/molecule/default/tests/test_default.py
+++ roles/prometheus/molecule/default/tests/test_default.py
@@ -1,9 +1,19 @@
 import os
+import pytest
 import testinfra.utils.ansible_runner
 
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("dirs", [
+    "/mnt/data/prometheus"
+])
+def test_dirs(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
 
 
 def test_user(host):
