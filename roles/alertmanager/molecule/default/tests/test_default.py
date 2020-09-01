eb65c3f4ed3d06a372b17d5e7b91b86a6844b2dd
nickherrig <neherrig@gmail.com>
2020-08-31 21:16:44 -0500

    Init alertmanager role with tests

---
+++ roles/alertmanager/molecule/default/tests/test_default.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_user(host):
+    assert host.group("alertmanger").exists
+    assert host.user("alertmanager").exists
+
+
+@pytest.mark.parametrize("dirs", [
+    "/mnt/data/alertmanager"
+])
+def test_dirs(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+@pytest.mark.parametrize('svc', [
+  'alertmanager'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
