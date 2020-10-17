18ec87223fe24ae63c10bec1a9eeaacd6805f17a
nickherrig <neherrig@gmail.com>
2020-10-16 21:09:19 -0500

    Finish ddclient install/configre role

--- roles/ddclient/molecule/default/tests/test_default.py
+++ roles/ddclient/molecule/default/tests/test_default.py
@@ -2,5 +2,25 @@ import os
 import pytest
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/ddclient.conf"
+])
+def test_dirs(host, files):
+    f = host.file(files)
+    assert f.is_file
+    assert f.exists
+
+
+@pytest.mark.parametrize('svc', [
+  'ddclient'
+])
+def test_svc(host, svc):
+    service = host.service(svc)
+
+    assert service.is_running
+    assert service.is_enabled
