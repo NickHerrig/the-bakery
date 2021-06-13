a378b2dd288738db0a4d750c854d6af97a9ec96b
nickherrig <neherrig@gmail.com>
2021-06-13 14:52:19 -0500

    Deploy DO lighthouse with terraform

---
+++ infra/infra.tf
@@ -0,0 +1,15 @@
+resource "digitalocean_ssh_key" "nick" {
+  name       = "nick"
+  public_key = file("${path.module}/ssh_keys/nick.pub")
+}
+
+resource "digitalocean_droplet" "nebula-lighthouse" {
+  image      = "ubuntu-20-04-x64"
+  name       = "nebula-lighthouse"
+  region     = "nyc1"
+  size       = "s-1vcpu-1gb"
+  monitoring = "true"
+  ssh_keys   = [
+    digitalocean_ssh_key.nick.fingerprint,
+    ]
+}
